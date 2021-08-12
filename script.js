924bdfe82c48fbc57b89759bb666683b8b0e6f6b
Melanie Schipper <78080525+MSchipper91@users.noreply.github.com>
2021-08-12 13:05:19 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const age = 18;
+
+if (age >= 18) {
+    console.log ('Je bent 18 jaar of ouder');
+} else {
+    console.log ('Je bent jonger dan 18 jaar');
+}
+
+console.log (age);
+
+
+const isFemale = true;
+
+if (isFemale) {
+    console.log ('Je bent een vrouw');
+} else {
+    console.log ('Je bent geen vrouw');
+}
+
+console.log (isFemale)
+
+
+const driverStatus = 'bob';
+const driverStatus = 'melanie'
+
+if (driverStatus == 'bob') {
+    console.log ('Jij bent de bob');
+} else {
+    console.log ('Jij bent dronken');
+}
+
+console.log (driverStatus);
